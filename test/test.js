e4ecdf892a4a886cb4089b844726d23c2218f46a
chuhongliang <chl01003698@163.com>
2021-02-25 10:32:17 +0800

    -

--- test/test.js
+++ test/test.js
@@ -1,1 +1,80 @@
 
+
+var arr = [9, 5, 7, 1, 2, 3];
+
+function bubbleSort(arr) {
+    var len = arr.length;
+    for (var i = 0; i < len - 1; i++) {
+        for(var j=0; j<len-1-i;j++){
+            console.log('j=>', j);
+            if(arr[j] > arr[j+1]){
+                var temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        console.log('arr=>', arr);
+    }
+    return arr;
+}
+// console.log('arr=>', bubbleSort(arr));
+
+
+
+
+function insertionSort(arr) {
+    var len = arr.length;
+    var preIndex, current;
+    for (var i = 1; i < len; i++) {
+        console.log('i=>', i);
+        preIndex = i - 1;
+        current = arr[i];
+        console.log('current=>', current);
+        while(preIndex >= 0 && arr[preIndex] > current) {
+            arr[preIndex+1] = arr[preIndex];
+            preIndex--;
+            console.log('arr=>', arr);
+        }
+        arr[preIndex+1] = current;
+        console.log('arr=>', arr);
+    }
+    return arr;
+}
+
+// console.log('arr=>', insertionSort(arr));
+
+
+function merge(left, right)
+{
+    var result = [];
+ 
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+ 
+    while (left.length)
+        result.push(left.shift());
+ 
+    while (right.length)
+        result.push(right.shift());
+ 
+    return result;
+}
+
+function mergeSort(arr) {  // 采用自上而下的递归方法
+    console.log('arr=>', arr);
+    var len = arr.length;
+    if(len < 2) {
+        return arr;
+    }
+    var middle = Math.floor(len / 2),
+        left = arr.slice(0, middle),
+        right = arr.slice(middle);
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+console.log('arr=>', mergeSort(arr));
--- test/testjisuan.js
+++ test/testjisuan.js
@@ -4,15 +4,26 @@ var array = [258, 281, 270, 298, 267, 281, 268, 276, 266, 289, 279, 358, 340, 37
 var desc = []; // 历史最大回撤
 var asc = []; // 历史最大上涨
 
-for (var x = 0; x < array.length - 1 ; x++) {
-    if(array[x] < array[x+1]){
-        var temp = Math.round((array[x+1] - array[x]) / array[x] * 100);
-        asc.push(temp/100);
-    }else{
-        var temp = Math.round((array[x] - array[x+1]) / array[x] * 100);
-        desc.push(temp/100);
+for (var x = 0; x < array.length - 1; x++) {
+    if (array[x] < array[x + 1]) {
+        var temp = Math.round((array[x + 1] - array[x]) / array[x] * 100);
+        asc.push(temp / 100);
+    } else {
+        var temp = Math.round((array[x] - array[x + 1]) / array[x] * 100);
+        desc.push(temp / 100);
     }
 }
 
-console.log('asc=> ', asc);
-console.log('desc=>', desc);
+// console.log('asc=> ', asc);
+// console.log('desc=>', desc);
+
+
+function getResult(num, year) {
+    for (var x = 1; x <= year; x++) {
+        num += Math.floor(num* 0.12);
+        console.log(x,'年','num=>', num);
+    }
+    return num;
+}
+
+console.log('num=>', getResult(1000000, 30));
