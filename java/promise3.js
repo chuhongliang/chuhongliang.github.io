ec995fcd41cdf64b3170f70eb79622848e1640ce
chuhongliang <13717807729@163.com>
2021-02-25 08:54:21 +0800

    -

--- java/promise3.js
+++ java/promise3.js
@@ -127,3 +127,4 @@ Promise.race = function (promises) {
 		});
 	});
 }
+
--- test/test.js
+++ test/test.js
@@ -1,22 +1,10 @@
 
 
-const Pet = function () {
+setTimeout(function () {
+	console.log('setTimeout');
+}, 1000)
 
-}
+setImmediate(function () {
+	console.log('setImmediate');
+}, 2000)
 
-Pet.drink = function () {
-
-}
-
-Pet.prototype.eat = function () {
-
-}
-
-
-
-
-let dog = new Pet();
-dog.eat();
-
-console.log('Pet.property=>', Pet.prototype);
-console.log('dog.__proto__=>', dog.__proto__);
